8e5fb058a5f69c6fd2105de1de79054174fe6964
tmf7 <tmf7@njit.edu>
2016-12-13 03:39:12 -0500

    Updated and unit-tested (passed all) HashIndex to use a user-defined hash function and iterator constructor/rebuild

--- HashIndexTest/HashIndexTest.cpp
+++ HashIndexTest/HashIndexTest.cpp
@@ -6,17 +6,18 @@
 #include <memory>
 #include <string>
 #include "HashIndex.h"
+#include <functional>
+
+#define TEST std::string
 #define UPTR(x) std::make_unique<int>(x)
 
 // unit test with int, std::string, std::unique_ptr<int> using both c-style array and dynamic array (std::vector<int>)
 // passed all tests. (future project: HashTable using pointers)
 int main() {
-	const int numElements = 15;
-	int ab[numElements] = { 4, 1, 7, 2, 1, 2, 2, 7, -1, 12, 566, 2, 567, 89, 2 };
-//	int a[numElements] = { 0, 1, 2, 2, 4, 5, 6, 7 };	// spread test
-	std::vector<int> a{ std::cbegin(ab), std::cend(ab) };
-/*
-	std::string a[numElements] = {
+//	int ab[] = { 4, 1, 7, 2, 1, 2, 2, 7, -1, 12, 566, 2, 567, 89, 2 };
+//	int abc[] = { 0, 1, 2, 2, 4, 5, 6, 7 };	// spread test
+/**/
+	std::string ab[] = {
 		"one",
 		"two",
 		"three four five six seven eight nine ten",
@@ -33,19 +34,23 @@ int main() {
 		"how are your\n",
 		"little test\n"
 	};
+	std::string abc[] = { "once", "twice", "three times", "an input", "tiwce", "twic" };
 
-	std::unique_ptr<int> a[numElements] = { UPTR(4), UPTR(1), UPTR(7), UPTR(2), UPTR(8), UPTR(2), 
-											UPTR(2), UPTR(7), UPTR(-1), UPTR(3), UPTR(566), 
-											UPTR(2), UPTR(567), UPTR(89), UPTR(2) };
-*/
-	eHashIndex hash;	// default constructed size of 8: works, initialHashSize(32) works
+//	std::unique_ptr<int> ab[] = { UPTR(4), UPTR(1), UPTR(7), UPTR(2), UPTR(8), UPTR(2), 
+//											UPTR(2), UPTR(7), UPTR(-1), UPTR(3), UPTR(566), 
+//											UPTR(2), UPTR(567), UPTR(89), UPTR(2) };
+	std::vector<TEST> a{ std::cbegin(ab), std::cend(ab) };
 
 	auto t1 = std::chrono::high_resolution_clock::now();
 
-	// generate random keys and put them into array "a"
-	// then add them to the eHashIndex
-	for (int i = 0; i < numElements; i++)
-		hash.Add(hash.GenerateKey(a[i]), i);
+	auto hasher = std::hash<TEST>{};
+	eHashIndex hash{ a.begin(), a.end(), hasher };	// default constructed size of 8: worked
+											// {initialHashSize == 32}: worked
+											// iterator build: worked
+
+	
+//	for (int i = 0; i < numElements; i++)	// build the eHashIndex manually: worked
+//		hash.Add(hash.GenerateKey(a[i]), i);
 
 	// duplicate key/index test (in-bounds, with same value): produces an inf-loop on Remove()
 	// wrong key/index test (in-bounds, with different array value): no inf-loop, but may skip avoid correct key/index pairs
@@ -61,9 +66,9 @@ int main() {
 
 	// search for pre-existing value: worked
 	// count how many with similar keys there are: worked
-	const int target = 2;	// this is what's sought
+	const TEST target = "123";	// this is what's sought
 	int iter = 0, found = 0;
-	int key = hash_a.GenerateKey(target);	
+	int hashkey = hasher(target);	
 
 	// removing an invalid key/index pair will corrupt the eHashIndex 
 	// (ie unable to find some indexes because indexChain is broken)
@@ -71,16 +76,29 @@ int main() {
 //	hash_a.Remove(key, 14);
 //	hash_a.Remove(key, 7);	
 
-	a.insert(a.begin() + 2, 777);
-	hash_a.InsertIndex(hash_a.GenerateKey(a[2]), 2);	// insertIndex for dynamic array: worked
+	a.insert(a.begin() + 2, "777");
+	hash_a.InsertIndex(hasher(a[2]), 2);	// insertIndex for dynamic array: worked
 
 	a.erase(a.begin() + 4);
-	hash_a.RemoveIndex(hash_a.GenerateKey(a[4]), 4);	// removeIndex for dynamic array: worked
+	hash_a.RemoveIndex(hasher(a[4]), 4);	// removeIndex for dynamic array: worked
 
-	for (int i = hash_a.First(key); i != -1; i = hash_a.Next(i)) {
+	for (int i = hash_a.First(hashkey); i != -1; i = hash_a.Next(i)) {
 		iter++;
 		if (a[i] == target) {
-			printf("Found: %i\n", a[i]);
+			printf("Found: %s\n", a[i].c_str());
+			found++;
+		}
+	}
+
+	a = std::vector<TEST> { std::cbegin(abc), std::cend(abc) };		// rebuild using new data: worked
+	hash_a.Rebuild(a.begin(), a.end(), hasher);
+	TEST target2 = "twice";
+	iter = 0, found = 0;
+	hashkey = hasher(target2);
+	for (int i = hash_a.First(hashkey); i != -1; i = hash_a.Next(i)) {	// search using rebuild hashIndex: worked
+		iter++;
+		if (a[i] == target2) {
+			printf("Found: %s\n", a[i].c_str());
 			found++;
 		}
 	}
